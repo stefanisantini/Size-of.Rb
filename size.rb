f4df067ffab129ba8d172f336734899415f90a96
Stéfani Santini Reis <110628639+stefanisantini@users.noreply.github.com>
2023-03-07 22:40:06 -0300

    Add files via upload

---
+++ size.rb
@@ -0,0 +1,24 @@
+puts "Digite o tamanho do seu pênis:"
+
+size = gets.chomp.to_i
+
+case size
+
+when 9..11
+    puts 'Você tem um pênis pequeno/mixuruca, melhor caprichar no oral'
+
+when 12..14
+puts 'Você tem um pênis médio, tem gente que gosta'
+
+when 15..17
+    puts 'Você tem um pênis grande'
+
+when 18..20
+    puts 'Você tem um pênis gigantesco/monstruoso'
+
+when 21..23
+    puts "Você tem anomalia ou é photoshop"
+
+else
+    puts 'Não foi possivel identificar'
+end
